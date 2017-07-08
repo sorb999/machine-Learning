62f7ffc0694578e245b605ad7cca6c5a8002700c
saurabh <asaurabh077@gmail.com>
2017-07-08 10:53:29 +0530

    added code to check libraries version

---
+++ checkVersions/checkVersions.py
@@ -0,0 +1,21 @@
+# checking version of various installed for Machine Learning
+
+
+# Python version
+import sys
+print('Python Version: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy Version: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy Version: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib Version: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas Version: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn Version: {}'.format(sklearn.__version__))
