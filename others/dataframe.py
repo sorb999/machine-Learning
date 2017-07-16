8f49a5907dbc87907775fe6ce5159a26b5ed183c
saurabh <asaurabh077@gmail.com>
2017-07-16 15:38:37 +0530

    added data sample pandas data frame

---
+++ others/dataframe.py
@@ -0,0 +1,21 @@
+
+# dataframe
+import numpy
+import pandas
+myarray = numpy.array([[1, 2, 3], [4, 5, 6]])
+rownames = ['a', 'b']
+colnames = ['one', 'two', 'three']
+mydataframe = pandas.DataFrame(myarray, index=rownames, columns=colnames)
+
+print('myarray:')
+print(myarray)
+
+print('rownames:')
+print(rownames)
+
+print('colnames:')
+print(colnames)
+
+print('mydataframe:')
+print(mydataframe)
+
