2e7961cb7f5416d16659b3cf01deae52d93fa931
saurabh <asaurabh077@gmail.com>
2017-07-17 22:55:56 +0530

    added description corerelation

--- others/descriptionCorelation.py
+++ others/descriptionCorelation.py
@@ -1,8 +1,7 @@
-# Statistical Summary
 import pandas
 url = "https://goo.gl/vhm1eU"
 names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
 data = pandas.read_csv(url, names=names)
-# description = data.describe()
-description = data.corr()
+description = data.describe()
+# description = data.corr()
 print(description)
