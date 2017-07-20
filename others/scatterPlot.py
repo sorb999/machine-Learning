e42b7e5fe1fdd02843cb2cccb66e4af321c253dd
saurabh <asaurabh077@gmail.com>
2017-07-20 21:03:09 +0530

    added scatter Plot Matrix

---
+++ others/scatterPlot.py
@@ -0,0 +1,9 @@
+# Scatter Plot Matrix
+import matplotlib.pyplot as plt
+import pandas
+from pandas.tools.plotting import scatter_matrix
+url = "https://goo.gl/vhm1eU"
+names = ['preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class']
+data = pandas.read_csv(url, names=names)
+scatter_matrix(data)
+plt.show()
